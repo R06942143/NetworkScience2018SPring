2d32415fdb80aa2e64166fc64f865cbf3bf6e5b9
籃聖皓 <37167564+R06942143@users.noreply.github.com>
2018-05-15 02:43:05 +0800

    Update kl.py

--- kl-algorithm/kl.py
+++ kl-algorithm/kl.py
@@ -9,7 +9,6 @@ import networkx as nx
 from networkx.utils import not_implemented_for
 from networkx.algorithms.community.community_utils import is_partition
 import time
-__all__ = ['kernighan_lin_bisection']
 
 
 def _compute_delta(G, A, B, weight):
@@ -81,46 +80,7 @@ def _kernighan_lin_pass(G, A, B, weight):
 
 # [docs]@not_implemented_for('directed')
 def kernighan_lin_bisection(G, partition=None, max_iter=100, weight='weight'):
-    """Partition a graph into two blocks using the Kernighan–Lin
-    algorithm.
 
-    This algorithm paritions a network into two sets by iteratively
-    swapping pairs of nodes to reduce the edge cut between the two sets.
-
-    Parameters
-    ----------
-    G : graph
-
-    partition : tuple
-        Pair of iterables containing an initial partition. If not
-        specified, a random balanced partition is used.
-
-    max_iter : int
-        Maximum number of times to attempt swaps to find an
-        improvemement before giving up.
-
-    weight : key
-        Edge data key to use as weight. If None, the weights are all
-        set to one.
-
-    Returns
-    -------
-    partition : tuple
-        A pair of sets of nodes representing the bipartition.
-
-    Raises
-    -------
-    NetworkXError
-        If partition is not a valid partition of the nodes of the graph.
-
-    References
-    ----------
-    .. [1] Kernighan, B. W.; Lin, Shen (1970).
-       "An efficient heuristic procedure for partitioning graphs."
-       *Bell Systems Technical Journal* 49: 291--307.
-       Oxford University Press 2011.
-
-    """
     # If no partition is provided, split the nodes randomly into a
     # balanced partition.
     for div in range(2,8):
